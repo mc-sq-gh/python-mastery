07c41629c17a6e5a1a8372ba90654c19a02d13bb
Mark Corwin <130515723+mc-sq-gh@users.noreply.github.com>
2023-08-02 18:15:08 -0400

    Exercise 1.4

--- code/ex1/pcost.py
+++ code/ex1/pcost.py
@@ -1,12 +1,22 @@
 # pcost.py
 
-total_cost = 0.0
+def portfolio_cost(filename):
+    total_cost = 0.0
 
-with open("../../Data/portfolio.dat", 'r') as f:
-    for line in f:
-        elems = line.split(' ')
-        shares = int(elems[1])
-        price = float(elems[2])
-        total_cost += shares * price
+    with open(filename, 'r') as f:
+        for line in f:
+            elems = line.split()
+            try:
+                shares = int(elems[1])
+                price = float(elems[2])
+            except ValueError as e:
+                print(f"Couldn't parse: {line.strip()}")
+                print(f"Reason: {e}")
+                continue
 
-print(total_cost)
+            total_cost += shares * price
+
+    return total_cost
+
+print("P1: "+ str(portfolio_cost("../../Data/portfolio.dat")))
+print("P3: "+ str(portfolio_cost("../../Data/portfolio3.dat")))
